a513e5611560d70c62ac80ef1b99e6cec713fa52
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-03-23 23:46:34 -0500

    Initial commit

---
+++ src/components/bento.svelte
@@ -0,0 +1,1 @@
+<section class="p-2 round-md bg-purple-300">hola</section>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Bento from "../components/bento.svelte";
+</script>
+<section class="grid grid-rows-4 grid-cols-6 p-8 bg-white dark:bg-gray-800 h-screen min-h-90">
+  <h1>Hola</h1>
+  <Bento />
+</section>
