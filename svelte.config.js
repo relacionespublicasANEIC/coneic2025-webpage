f813fa0ba6e30d7ce0571420ff30ed2c611699df
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-03-24 18:11:01 -0500

    Configure new aliases path

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,10 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-export default { preprocess: vitePreprocess(), kit: { adapter: adapter() } };
+export default {
+    alias: {
+        "@components/*": "./src/components/*",
+        "@icons/*": "./src/icons/*"
+    },
+    preprocess: vitePreprocess(),
+    kit: { adapter: adapter() }
+};
