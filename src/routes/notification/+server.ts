e943682bfb630c4db6dca3b2129c5beebf3fed58
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-08-24 00:50:36 -0500

    Change logic to save only transation on APPROVED transactions.

--- src/routes/notification/+server.ts
+++ src/routes/notification/+server.ts
@@ -3,31 +3,34 @@ import { REDIS_URL } from "$env/static/private";
 import { error, json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
+const redis = await createClient({ url: REDIS_URL }).connect();
 
 // Defines a handler for events from wompi.
 // https://docs.wompi.co/docs/colombia/eventos/
-
-interface WompiEvent {
-    "event": "transaction.updated" | "nequi_token.updated" | "bancolombia_transfer_token.updated"
-    "data": {
-        "transaction": {
-            "id": string,
-            [key: string]: string,
-        }
-    }
-}
-
-const redis = await createClient({ url: REDIS_URL }).connect();
-
 export const POST: RequestHandler = async ({ request }) => {
-    let body: WompiEvent = await request.json();
+    let body = await request.json().catch(() => error(400, "Body is void or not json."));
 
+    // Check the body is a WompiEvent.
+    if (!body?.event || !body?.data?.transaction) {
+        return error(400, "Body json has a invalid shape.");
+    };
+
+    // Handle the WompiEvent.
     if (body.event !== "transaction.updated") {
-        console.log("Useless event");
-        return json({}, { status: 200 });
+        return json({ "message": "Skipped event." }, { status: 200 });
     };
 
     let transaction = body.data.transaction;
-    await redis.json.set(transaction.id, "$", transaction).catch((_) => error(400, "Unable to write in db"));
-    return json({}, { status: 200 });
+    if (transaction.status === "DECLINED") {
+        await redis.incr("declined_request").catch((_) => console.error("Counter did not increment."));
+        return json({ "message": "Total declined requests incresed by one." }, { status: 200 });
+    };
+
+    if (transaction.status === "APPROVED") {
+        await redis.incr("aproved_request").catch((_) => console.error("Counter did not increment."));
+        await redis.json.set(transaction.id, "$", transaction).catch((_) => error(400, "Unable to write in db"));
+        return json({ "message": "Aproved request was saved in database." }, { status: 200 });
+    };
+
+    return json({ "message": "Event was not handled." }, { status: 200 });
 }
