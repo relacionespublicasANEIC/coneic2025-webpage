dc06a1fde7eb33a04f44c196edc32880fa4fe0a4
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-08-25 19:48:32 -0500

    Rename component and change its logic.

---
+++ src/components/image-card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let src: string;
+    export let description: string;
+</script>
+
+<figure class="shadow-md mb-4 p-2 rounded-md">
+    <img class="mx-auto object-cover overflow-hidden w-full aspect-video" {src} alt="Fotografia" />
+    <figcaption class="p-2">
+        <p class="text-sm text-center">{description}</p>
+    </figcaption>
+</figure>
