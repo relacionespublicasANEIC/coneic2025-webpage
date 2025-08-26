2066eaa6b741cbb75f7ac986e0b16e14f24a31bf
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-08-25 19:49:11 -0500

    Add new component for typing text effect

---
+++ src/components/typing-text.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import lines from "../data/lines.json";
+
+    let index = $state(0);
+    let direction = $state(1);
+    let line = $derived(lines[index]);
+
+    let charIndex = $state(1);
+    let textSlice = $derived(line.slice(0, charIndex));
+
+    function updatesIndexes() {
+        if (charIndex >= line.length) {
+            index += direction;
+            direction = index == 0 || index == lines.length ? 1 : -1;
+            charIndex = 0;
+        } else {
+            charIndex++;
+        }
+    }
+
+    onMount(() => {
+        const thisInterval = setInterval(updatesIndexes, 65);
+        return () => clearInterval(thisInterval);
+    });
+</script>
+
+<span>{textSlice}</span>
