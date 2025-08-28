76b60ea18731d15332291d3030499eddd34309dd
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-08-28 02:52:12 -0500

    Update payment result page.

--- src/routes/compra/+page.svelte
+++ src/routes/compra/+page.svelte
@@ -1,84 +1,58 @@
 <script lang="ts">
     import "../../styles.css";
-    import qrcode from "qrcode-generator";
+    import type { PageProps } from "./$types";
+    import Bill from "../../icons/bill.svelte";
     import badges from "../../data/badges.json";
 
-    export let data;
+    let { data }: PageProps = $props();
 
-    function createQr(data: string): string {
-        let qr = qrcode(0, "M");
-        qr.addData(data);
-        qr.make();
-        return qr.createDataURL(10, 0);
-    }
+    const fillColor: { [key: string]: string } = {
+        PENDING: "fill-gray-500",
+        DECLINED: "fill-red-600",
+        ERROR: "fill-red-600",
+        VOIDED: "fill-gray-500",
+        APPROVED: "fill-green-500",
+    };
 
-    function getBadge(paymentId: string): [string, string] {
-        if (paymentId === "" || paymentId === undefined) {
-            return ["", ""];
-        }
+    const statusTitle: { [key: string]: string } = {
+        PENDING: "pendiente",
+        DECLINED: "fallida",
+        ERROR: "fallida",
+        VOIDED: "cancelada",
+        APPROVED: "completada",
+    };
 
-        let bagde_json = badges.find((i) => i.payment === paymentId)!;
-        return [bagde_json.title, bagde_json.color];
-    }
-
-    let customer = data?.message?.trasanction?.customer_data;
-    let qr_data = [data?.message?.hash, customer?.full_name, customer?.legal_id];
-    let qr_url = createQr(qr_data.join("|"));
-    let bagde_info = getBadge(data?.message?.trasanction?.payment_link_id);
+    const messageInfo: { [key: string]: string } = {
+        PENDING: "Estamos esperando aún que la transacción se complete.",
+        DECLINED: data.status_message + ".",
+        ERROR: data.status_message + ".",
+        VOIDED: "La transacción fue cancelada.",
+        APPROVED: "A tu correo te llegará la confirmación de tu compra.",
+    };
 </script>
 
-{JSON.stringify(data)}
+<svelte:head>
+    <title>CONEIC 2025 | Resultado de compra online de carnet</title>
+</svelte:head>
 
-<section class="mt-6">
-    {#if data.status !== "APPROVED"}
-        <section
-            class="w-1/2 max-w-3/4 p-8 m-auto flex flex-col border-2 border-barranquilla-red rounded-md">
-            <h1 class="text-6xl text-center text-barranquilla-red">TRANSACCIÓN {data.title}</h1>
-            <div class="mt-4">
-                <p class="text-2xl text-center">¿Qué pasó?</p>
-                <h2 class="text-4xl text-center">{data.message}</h2>
-            </div>
-            <div class="flex justify-center mt-6">
-                <a class="border-2 p-2 rounded-md bg-barranquilla-red" href="/#carnets"
-                    >Vuelve a la página inicial para intertarlo de nuevo</a>
-            </div>
-        </section>
-    {:else}
-        <section class="w-full mt-12">
-            <section class="print:w-3/4 w-1/4 m-auto p-8 rounded-md {bagde_info[1]} aspect-17/22">
-                <section class="relative bg-white rounded-md p-4 h-full flex flex-col">
+<section class="p-8">
+    <section>
+        <div class="w-8 {fillColor[data.status]}">
+            <Bill />
+        </div>
 
-                    <div class="w-16 absolute right-4">
-                        <img src="coneic2025.webp" alt="">
-                    </div>
+        <h1 class="text-xl">Transacción {statusTitle[data.status]}</h1>
+        <h2>{messageInfo[data.status]}</h2>
+        <br />
 
-                    <div class="grow">
-                        <div>
-                            <p>Nombre</p>
-                            <p class="text-3xl">{customer.full_name}</p>
-                        </div>
-                        <div>
-                            <p>Documento</p>
-                            <p class="text-3xl">
-                                {customer.legal_id_type + " " + customer.legal_id}
-                            </p>
-                        </div>
+        {#if data.status === "APPROVED"}
+            <p>Identificador de compra</p>
+            <p class="text-xl">{data.id}</p>
 
-                        <p>Universidad</p>
-                        <p class="text-3xl">{customer.customer_references[0].value}</p>
-                    </div>
-                    <div>
-                        <img class="block w-1/2 m-auto" src={qr_url} alt="QR Code" />
-                        <p class="text-center">{bagde_info[0]}</p>
-                    </div>
-                </section>
-            </section>
-
-            <p class="text-center mt-10 print:hidden">
-                Tómale una foto a tu carnet o <button
-                    onclick={() => window.print()}
-                    class="{bagde_info[1]} text-white p-1 cursor-pointer">guardalo en PDF</button>
+            <p>Carnet</p>
+            <p class="text-xl">
+                {badges.find((i) => i.payment === data.payment_link_id)?.title}
             </p>
-        </section>
-    {/if}
+        {/if}
+    </section>
 </section>
