37187da3f70ab377eb6dc2769fb7f6999532b953
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-10-31 02:22:31 -0500

    Get user info from its email.

--- src/routes/actividades/+page.server.ts
+++ src/routes/actividades/+page.server.ts
@@ -23,30 +23,55 @@ export const actions: Actions = {
     },
     find_user: async ({ request }) => {
         let formData = await request.formData();
-        let number = formData.get("buy");
+        let email_reference = formData.get("buy");
 
-        if (!number) {
+        if (!email_reference) {
             return fail(400, { message: "No number" });
         };
 
-
-
         const redis = await createClient({ url: REDIS_URL }).connect().catch((_) => error(500, "Unable to connect to db."));
-        // try to get by transaction number
+
+        // we will search for the email in the list
         let info;
+        let ref;
+        let pre2 = pre;
         try {
-            let a = await redis.hGet(pre + "custom-data-list", number!.toString());
-            info = JSON.parse(a!);
-        } catch {
-            return fail(400, {});
-        };
+            // first, we'll look for email position
+            let rr = await redis.lPos(pre2 + "emailed-id-list", email_reference.toString());
+            if (!rr) throw "";
+
+            // luego, we'll look for transaction number, relative to email.
+            let transaction_number = await redis.lIndex(pre2 + "emailed-id-list", rr + 1);
+            if (!transaction_number) throw "";
+
+            // with email, we'll search transaction information
+            let transaction_info = await redis.hGet(pre2 + "transaction-data-list", transaction_number);
+            if (!transaction_info) throw "";
+            let d = JSON.parse(transaction_info);
+
+            // then we'll transaction reference
+            let transaction_ref: string = (d?.invoice_id || d?.reference);
+            if (!transaction_ref) throw "";
+
+            // with reference, we'll get client info
+            let user_info = await redis.hGet(pre2 + "custom-data-list", transaction_ref);
+            if (!user_info) throw "";
+            let user_info_live = JSON.parse(user_info);
+            info = user_info_live;
+            ref = transaction_ref;
+
+        } catch (e) {
+            console.error("algo falló");
+            fail(400);
+        }
+
 
         return {
-            reference: number!.toString(),
-            name: info!.user.name,
+            reference: ref,
+            name: info.user.name,
             email: info.user.email,
             university: info.user.university,
-            hasArl: true
+            hasArl: false
         };
     }
 }
